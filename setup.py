89eba61b88d0ddae3f0fb25373a97d445704df9d
datadavev <datadavev@users.noreply.github.com>
2018-10-20 11:10:37 -0400

    Update dependencies and version

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@ from setuptools import find_packages, setup
 
 setup(
   name='d1_examples',
-  version='2.0.0',
+  version='2.1.0',
   packages=find_packages(),
   include_package_data=True,
   zip_safe=False,
   install_requires=[
-    'flask',
     'requests',
-    'd1_common',
+    'flask',
+    'flask-headers',
+    'gunicorn',
+    'pyopenssl',
+    'dataone.common',
   ],
 )
